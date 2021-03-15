81cd4c4cce0bf91c5598d36138d6150e5f93f4e4
takanoha001 <72856896+takanoha001@users.noreply.github.com>
2021-03-16 07:19:28 +0900

    forgot to add them

---
+++ src/AppClass.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class AppClass extends Component {
+  render() {
+    return (
+      <div className="AppClass">
+        <p> printing from AppClass.js </p>
+      </div>
+    );
+  }
+}
+
+export default AppClass;
---
+++ src/AppFunctional.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function AppFunctional() {
+  return (
+    <div className="AppFunctional">
+      <p>printing from AppFunctiona.js</p>
+    </div>
+  );
+}
+
+export default AppFunctional;
