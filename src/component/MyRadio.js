1098d8a024cdc7fc0d535f31ce8fa15e876ad63f
takanoha <takanoha001@gmail.com>
2021-04-11 00:16:11 +0900

    generate radio buttons from map array

--- src/component/MyRadio.js
+++ src/component/MyRadio.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 
+const members = ["Araki", "Ibata", "Fukutome", "Woods", "Alex", "Tatsunami"];
+
 export default class MyRadio extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       sex: "male",
+      lastName: "",
     };
     this.handleChange = this.handleChange.bind(this);
     this.show = this.show.bind(this);
   }
 
   handleChange(e) {
-    console.log(`handleChange(): ${this.state.sex}`);
+    console.log("handleChange() e:" + e.target.value);
 
     this.setState({
       [e.target.name]: e.target.value,
@@ -20,15 +23,84 @@ export default class MyRadio extends Component {
   }
 
   show() {
-    console.log(`show(): ${this.state.sex}`);
+    //console.log(`show(): ${this.state.sex}`);
   }
 
   render() {
+    console.log("render: " + this.state.lastName);
+
+    return (
+      <form>
+        <div>
+          <h2>MyRadio.js</h2>
+          {/* 
+          <label htmlFor="sex_male">male</label>
+          <input
+            id="sex_male"
+            name="sex"
+            type="radio"
+            value="male"
+            checked={this.state.sex === "male"}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="sex_female">female</label>
+          <input
+            id="sex_female"
+            name="sex"
+            type="radio"
+            value="female"
+            checked={this.state.sex === "female"}
+            onChange={this.handleChange}
+          />
+          <br /> */}
+
+          {/* {this.state.current_alternative.map((item) => (
+            <input key={item.id} type="radio">
+              <label>{item.content}</label>
+            </input>
+          ))} */}
+
+          {members.map((item) => (
+            <React.Fragment>
+              <label htmlFor={item}> {item}</label>
+              <input
+                name="lastName"
+                key={item}
+                id={item}
+                value={item}
+                type="radio"
+                onChange={this.handleChange}
+              />
+            </React.Fragment>
+          ))}
+        </div>
+        <div></div>
+      </form>
+
+      // <form>
+      //   <h2>MyRadio.js</h2>
+      //   <fieldset>
+      //     <input
+      //       type="radio"
+      //       name="selectedRadio"
+      //       th:value="${item.key}"
+      //       th:text="${item.value}"
+      //       th:field="*{selectedRadio}"
+      //     />
+      //   </fieldset>
+      //   <button type="button" onClick={this.show}>
+      //     send
+      //   </button>
+      // </form>
+    );
+  }
+
+  render2() {
     return (
       <form>
         <h2>MyRadio.js</h2>
-        <fieldset>
-          <legend>sex</legend>
+        {/*<fieldset>
+         <legend>sex</legend>
 
           <label htmlFor="sex_male">male</label>
           <input
@@ -59,7 +131,7 @@ export default class MyRadio extends Component {
             checked={this.state.sex === "other"}
             onChange={this.handleChange}
           />
-        </fieldset>
+        </fieldset> */}
         <button type="button" onClick={this.show}>
           send
         </button>
